5252cedd688a4f345458fadf6bb5aec2b2de133b
TheyCallMeAndyBro <andy060104@gmail.com>
2023-09-26 20:44:48 +0800

    add serach function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const restaurants = require('./public/jsons/restaurant.json').results
 
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
@@ -14,14 +15,25 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
-  res.render('index');
+  const keyword = req.query.keyword?.trim()
+  const matchedRestaurant = keyword ? restaurants.filter(ra => {
+    if (typeof ra.name === 'string' && typeof ra.category === 'string') {
+      return ra.name.toLowerCase().includes(keyword) || ra.category.toLowerCase().includes(keyword)
+    }
+    else return false
+    //filter函數會過濾true以外的元素，當return返回false時會自動被過濾轉為空字串
+    //這就是為甚麼當輸入餐廳以外的字時不會搜尋到任何東西
+  }) : restaurants
+
+  res.render('index', { restaurants: matchedRestaurant, keyword });
 })
 
-app.get('/restaurant/:id', (req, res) =>{
+app.get('/restaurant/:id', (req, res) => {
   const id = req.params.id
-  res.send(`${id}`)
+  const restaurant = restaurants.find(ra => ra.id.toString() === id)
+  res.render(`show`, { restaurant })
 })
 
-app.listen(port, function(req, res){
+app.listen(port, function (req, res) {
   console.log(`express server is running on http:/localhost:${port}`);
 })
